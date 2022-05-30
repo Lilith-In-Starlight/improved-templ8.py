a345b40e2d0201d7ae12654dd715fdca232fe3e9
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-05-30 01:14:29 -0500

    Parser for IFKEYS

--- src/templ8/blessing.py
+++ src/templ8/blessing.py
@@ -92,3 +92,120 @@ def parse_content(content, ext):
 	else:
 		raise Exception("Can't recognize the extension in " + os.join(subdir, file))
 		return ""
+
+
+def into_html(content, keys):
+	base = state.basehtml_content
+	if "CUSTOMBASE" in keys:
+		if os.path.exists(keys["CUSTOMBASE"]):
+			base = open(keys["CUSTOMBASE"], "r").read()
+		else:
+			raise Exception(os.path.join(subdir, file) + " uses a CUSTOMBASE that doesn't exist")
+	
+	
+	
+	output = base.replace("##CONTENT##", content)
+	
+ifkey_start = "$$IF_"
+fkey_end = "$$END$$"
+
+
+class Token:
+	def __init__(self, end, type, text, start):
+		self.end = end
+		self.type = type
+		self.text = text
+		self.start = start
+	def __repr__(self):
+		return f"[ C: {self.start} - {self.end} T: {self.type} Tx: {self.text} ]"
+	def __eq__(self, other):
+		return (self.end == other.end and self.start == other.start)
+
+
+def parts(input_base):
+	start = 0
+	tokens = {}
+	while input_base.find(ifkey_start, start) != -1 or input_base.find(fkey_end, start) != -1:
+		token_start = 0
+		token_end = 0
+		token_type = "void"
+		if input_base.find(ifkey_start, start) < input_base.find(fkey_end, start) and input_base.find(ifkey_start, start) != -1:
+			token_start = input_base.find(ifkey_start, start)
+			token_end = input_base.find("$$", token_start + 1) + 2
+			token_type = "IF"
+		else:
+			token_start = input_base.find(fkey_end, start)
+			token_end = token_start + len(fkey_end)
+			token_type = "END"
+			
+		
+		tokens[token_start] = Token(token_end, token_type, input_base[token_start:token_end], token_start)
+		
+		start = token_end 
+	return tokens
+
+
+
+
+
+def funkeys(input_base, keys, tokens):
+	tokpos = sorted(tokens.keys())
+	index = 0
+	for_deletion = []
+	while True:
+		ctoken = tokens[tokpos[index]]
+		if tokpos[index+1] in tokens:
+			ntoken = tokens[tokpos[index+1]]
+			if ntoken.type == "END":
+				if ctoken.type == "IF":
+					if_key = ctoken.text[len(ifkey_start):-2]
+					if if_key in keys:
+						for_deletion.append(ctoken)
+						for_deletion.append(ntoken)
+					else:
+						for_deletion.append(Token(ntoken.end, "CONTENT", "aaa", tokpos[index]))
+					
+					tokpos.pop(index)
+					tokpos.pop(index)
+					index -= 1
+			else:
+				index += 1
+		if index >= len(tokpos) - 1:
+			break
+	
+	
+	delclumps = [for_deletion[0]]
+	
+	old_delclumps = for_deletion
+	while True:
+		index = 0
+		for tk in old_delclumps:
+			ntk_start = tk.start
+			ntk_end = tk.end
+			if delclumps[index].start >= ntk_start and delclumps[index].end <= ntk_end:
+				delclumps[index].start = ntk_start
+				delclumps[index].end = ntk_end
+			else:
+				delclumps.append(Token(ntk_end, "CONTENT", "AAA", ntk_start))
+				index += 1
+		if old_delclumps == delclumps:
+			break
+		else:
+			old_delclumps = delclumps
+			delclumps = [old_delclumps[0]]
+			
+	
+	
+	delbars = []
+	out = input_base
+	for tk in delclumps:
+		start = tk.start
+		end = tk.end
+		for i in delbars:
+			if start > i[0]:
+				start -= i[1]
+			if end > i[0]:
+				end -= i[1]
+		out = out[:start] + out[end:]
+		delbars.append([start, end - start])
+	return out
